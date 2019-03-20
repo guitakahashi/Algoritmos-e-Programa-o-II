79ca6f90258535f0f87630c622cfcdf7f23561e3
GUILHERME SERRATI TAKAHASHI <181220041@fspoa.br>
2019-03-20 20:24:54 -0300

    first commit

---
+++ Desktop/Pygame.py
@@ -0,0 +1,9 @@
+
+import time
+import pygame
+pygame.init()
+tela = pygame.display.set_mode([800, 600])
+pygame.display.set_caption('Oi, sou o PyGame')
+tela.fill([0, 0, 0])
+pygame.display.flip()
+time.sleep(10)
